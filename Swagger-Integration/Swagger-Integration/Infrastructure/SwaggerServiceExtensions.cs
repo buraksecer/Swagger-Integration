ff25fd6229ce1a3e71988751725cc7c185d88543
Burak Seçer <brkscr@hotmail.com.tr>
2019-11-05 10:03:20 +0300

    Infrastructure - Swagger Service Extension

---
+++ Swagger-Integration/Swagger-Integration/Infrastructure/SwaggerServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Swagger_Integration.Infrastructure
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1.0", new Info
+                {
+                    Title = "Swagger API v1.0",
+                    Version = "v1.0",
+                    Contact = new Contact()
+                    {
+                        Name = "Burak SEÇER",
+                        Url = "http://buraksecer.com",
+                        Email = "burakscr@gmail.com"
+                    }
+                });
+            });
+            return services;
+        }
+    }
+}
